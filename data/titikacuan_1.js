f30491ee89dd97c5bf571473c59dd7c6f1f4e8f4
KanayaNadira <75061607+KanayaNadira@users.noreply.github.com>
2020-12-16 07:15:26 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,8 @@
+var json_titikacuan_1 = {
+"type": "FeatureCollection",
+"name": "titikacuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 107.564185332621307, -6.913899487702829 ] } }
+]
+}
